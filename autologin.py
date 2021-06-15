6524f92ffa6b85cf9153c4e63d660542337507fd
ThisisKUNMENG <85155366+ThisisKUNMENG@users.noreply.github.com>
2021-06-15 21:20:50 +0800

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -2,14 +2,23 @@ from selenium import webdriver
 import time
 from selenium.webdriver.support.select import Select
 from tkinter import messagebox
+
+username=""
+#用户名
+password=""
+#密码
+domain=""
+#认证域
+
+
 driver = webdriver.Chrome()
 driver.get('http://10.102.250.13/index.php?url=aHR0cDovL3d3dy5tc2Z0Y29ubmVjdHRlc3QuY29tL3JlZGlyZWN0')
 time.sleep(1)
 if driver.find_element_by_id("loginForm").is_displayed()==True :
-    driver.find_element_by_id("username").send_keys("18817265295")
+    driver.find_element_by_id("username").send_keys(username)
     option=driver.find_element_by_id("domain")
-    Select(option).select_by_value("CMCC")
-    driver.find_element_by_id("password").send_keys("591887")
+    Select(option).select_by_value(domain)
+    driver.find_element_by_id("password").send_keys(password)
     time.sleep(1)
     driver.find_element_by_id('login').click()
     time.sleep(3)
